704730ed90fd706f889320cde1c8e563af0fee32
prathmesh More <prathmesh@hakimo.ai>
2025-07-14 12:49:08 +0530

    Minor changes related to db

--- src/database/connection.py
+++ src/database/connection.py
@@ -7,7 +7,7 @@ def connect_to_database(config):
     try:
         connection = mysql.connector.connect(**config)
         if connection.is_connected():
-            logger.info(f"Connected to: {config['database']}")
+            logger.info(f"Connected to: {config['database']}, user: {config['user']}, host: {config['host']}")
             return connection
     except Error as e:
         logger.error(f"Connection error: {e}")
